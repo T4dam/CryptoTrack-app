6f74b296b71e75e60442f2b0577c80599d71e12e
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-07-29 11:39:27 +0300

    trending complete

---
+++ src/components/hero/trending-card-row.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './hero.css';
+
+const TrendingCardRow = ({ number, image, name, ticker, price }) => {
+	return (
+		<div className="coin-wrapper">
+			<div className="grid">
+				<div className="center hide-mobile">{number}</div>
+				<img className="center scale" src={image} alt="" />
+				<div className="center">{name}</div>
+				<div className="center opacity hide-mobile">{ticker}</div>
+			</div>
+			<div>{price}</div>
+		</div>
+	);
+};
+
+export default TrendingCardRow;
--- src/components/hero/trending.js
+++ src/components/hero/trending.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import './hero.css';
+import TrendingCardRow from './trending-card-row';
 
 const Trending = ({ coins }) => {
-	console.log(coins.coins);
 	return (
-		<div>
-			<div>
+		<div className="wrapper">
+			<div className="title flex">
 				<h3>Trending</h3>
+				<h3 className="opacity">Price/BTC</h3>
 			</div>
-			<div>
+			<div className="flex-map">
 				{coins.coins &&
 					coins.coins.map((item) => {
 						// prettier-ignore
-						return <div key={item.item.id}>{item.item.name}</div>
+						return (<>
+                        <TrendingCardRow key={item.item.id}
+
+                        number={item.item.market_cap_rank}
+                        image={item.item.small}
+                        name={item.item.name}
+                        ticker={item.item.symbol}
+                        price={item.item.price_btc.toFixed(6)}
+                        />
+                        
+                        
+                        </>
+                        )
 					})}
 			</div>
 		</div>
