d390e26757053b33ae7306d4be7a3a56f6607e6f
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-07-29 19:16:35 +0300

    hover css on hero coins

--- src/components/hero/coin-card.js
+++ src/components/hero/coin-card.js
@@ -3,14 +3,16 @@ import './hero.css';
 
 const CoinCard = ({ number, image, name, ticker, price }) => {
 	return (
-		<div className="coin-wrapper">
-			<div className="grid">
-				<div className="center hide-mobile">{number}</div>
-				<img className="center scale" src={image} alt="" />
-				<div className="center">{name}</div>
-				<div className="center opacity hide-mobile">{ticker}</div>
+		<div className="fullwidth">
+			<div className="coin-wrapper coinshover">
+				<div className="grid">
+					<div className="center hide-mobile">{number}</div>
+					<img className="center scale" src={image} alt="" />
+					<div className="center">{name}</div>
+					<div className="center opacity hide-mobile">{ticker}</div>
+				</div>
+				<div>{price}</div>
 			</div>
-			<div>{price}</div>
 		</div>
 	);
 };
