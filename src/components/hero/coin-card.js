3b0285b08eaced94f44c1e9a1bfbf9592e570f1c
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-08-14 19:14:49 +0300

    styles for tables and background

--- src/components/hero/coin-card.js
+++ src/components/hero/coin-card.js
@@ -3,14 +3,16 @@ import './hero.css';
 
 const CoinCard = ({ number, image, name, ticker, price }) => {
 	return (
-		<div className="coin-wrapper coinshover">
-			<div className="grid">
+		<div className="coin-wrapper coinshover padd ">
+			<div className="grid ">
 				<div className="center hide-mobile">{number}</div>
 				<img className="center scale" src={image} alt="" />
 				<div className="center">{name}</div>
 				<div className="center opacity hide-mobile">{ticker}</div>
 			</div>
-			<div>{price} ₿</div>
+			<div className="flexSide">
+				{price} <span className="padding-left">₿</span>
+			</div>
 		</div>
 	);
 };
