0723e782e1dc8ffdf82babec1c160f02a5db51ef
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-10-20 11:01:46 +0300

    more coins

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 	const [coin, setCoin] = useState([]);
 
 	const url =
-		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=Eur&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=Eur&order=market_cap_desc&per_page=35&page=1&sparkline=false';
 
 	useEffect(() => {
 		axios
